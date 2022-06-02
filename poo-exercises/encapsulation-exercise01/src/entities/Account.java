5db8243bec731efe4630f180005ab9a03eb802f5
mdorici <marianadorici@gmail.com>
2022-06-02 00:53:32 -0300

    Encapsulation exercise

---
+++ poo-exercises/encapsulation-exercise01/src/entities/Account.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Account {
+    private int accountNumber;
+    private String holder;
+    private double balance;
+
+    public Account(int accountNumber, String holder) {
+        this.accountNumber = accountNumber;
+        this.holder = holder;
+    }
+
+    public Account(int accountNumber, String holder, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5.0;
+    }
+
+    public String toString() {
+        return "Account " + accountNumber
+                + " , Holder: " + holder
+                + " , Balance: $ " + String.format("%.2f", balance);
+    }
+}
