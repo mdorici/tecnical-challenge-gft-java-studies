5e03d934c92fce254909fca6f07986b65edc57f3
mdorici <marianadorici@gmail.com>
2022-06-01 22:08:33 -0300

    Update challenges

--- challenges-gft-start4-java/challenge10/src/application/Program.java
+++ challenges-gft-start4-java/challenge10/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
 //Desafio intermediário - Área Direita
@@ -10,23 +11,31 @@ public class Program {
 
         double sum = 0.0;
         double average = 0.0;
-        char O = sc.next().charAt(0);
+        int count = 0;
+        char O = sc.next().toUpperCase().charAt(0);
 
         double[][] M = new double[12][12];
 
         for(int i = 0; i < M.length; i++) {
-            for(int j = 0; j <= M.length; j++) {
+            for(int j = 0; j <= M[i].length; j++) {
                 M[i][j] = sc.nextDouble();
             }
         }
 
         for(int i = 0; i < M.length; i++) {
-            for(int j = 0; j < M.length; j++) {
-                if (j > i && j > )
+            for(int j = 0; j < M[i].length; j++) {
+                if (j > i && j > M.length -i -1) {
+                    sum += M[i][j];
+                    count++;
+                }
             }
         }
 
+        if(O == 'M') {
+            sum = sum/count++;
+        }
 
+        System.out.printf("%.1f", sum);
 
         sc.close();
     }
